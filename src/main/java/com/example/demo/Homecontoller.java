bbbca2df2599bae8ae905d7a1e97394b6ea21fa9
unknown <ryan77ryan7777@gmail.com>
2026-06-28 13:47:21 +0530

    sb code

---
+++ src/main/java/com/example/demo/Homecontoller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+package com.pillpal.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class Homecontroller {
+
+
+	@GetMapping("/")
+	public String home(){
+
+		return "index";
